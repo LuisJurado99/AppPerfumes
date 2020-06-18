d28bd9fdd7611c7e8f94c76973f4c33a63007004
luisj <luisjurado9924@gmail.com>
2020-06-17 22:14:14 -0500

    Se envia a comprar con error

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-var express = require('express');
-var router = express.Router();
 var request = require('request');
 
 router.get('/', function (req, res, next) {
@@ -13,16 +9,12 @@ router.get('/', function (req, res, next) {
     res.render('index', { data: JSON.parse(body) });
   });
 });
-router.post('/web/interfaz/buscar', (req, res, next) => {
-  request('http://localhost:3000/apiPerfume', (err, resp, body) => {
 
-    //res.render('perfil',{'datos':JSON.parse(body)});
-  });
-});
-
-router.get('/comprar/:id', (req, res, next) => {
+router.get('/compras/:id', (req, res, next) => {
   request('https://apiperfumetecjol.herokuapp.com/apiPerfume/'+req.params.id, (err, response, body) => {
     console.log(body);
+
+    res.render('compras', { data: JSON.parse(body) });
   });
 });
 
