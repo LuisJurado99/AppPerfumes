55ab13b911c13a4e8ef660a5b16cc50eae05ab35
cfonseca2 <cfonseca2@outlook.com>
2020-06-17 20:09:23 -0500

    Se crea interfaz compras

--- routes/index.js
+++ routes/index.js
@@ -19,58 +19,17 @@ router.post('/web/interfaz/buscar',(req,res,next)=>{
         //res.render('perfil',{'datos':JSON.parse(body)});
     });
   });
-/*
-router.post('/buscar',(req,res,next)=>{
-    request('http://localhost:3000/apiPerfume',)
-    Perfume.findOne({'nombre':req.body.buscar},(err,datos)=>{
-      if(err){
-        res.render('error',{title:'error'});
-      } 
-      else  {
-        console.log(datos);
-        if (datos==null){
-          res.status(200).json(datos);
-        }else{
-          res.render('buscar',{title:'buscar',datos:datos});
-        }
-      }
-    });
-  });*/
-/*router.patch('/actualizar/:perfumeId', (req,res,next)=>{
+
+  router.get('/comprar',(req,res,next)=>{
     console.log(req.body);
-    console.log(req.params.perfumeId);
-    Perfume.findOneAndUpdate( {'_id':req.params.perfumeId },{$set:req.body}, (err,datos)=>{
-      if(err) res.status(400).json({mensaje:"Error de Api"})
-      else res.status(200).json(datos)
-    });
-  });
-  
-router.get('/', function(req, res, next) {
-    Perfume.find( {} , (err,datos)=>{
-        console.log(err);
-        
-      if(err) res.render('error',{title:'Error', error:err})
-      else res.render('index',{datos: datos, title: "Perfumes"})
-    });  
-});
-router.post('/agregar',(req,res,next)=>{
-    var perfume=Perfume({
-      nombre: req.body.nombre,
-      marca: req.body.marca,
-      aroma: req.body.aroma,
-      cantidad: req.body.cantidad,
-      precio: req.body.precio,
-      tipo: req.body.tipo,
-    });
-    console.log(req.body);
+    console.log(res.body);
     
-    perfume.save((err,datos)=>{
-      if(err) res.render('error',{message: err});
-      else res.status(201).json(datos);
-    });
-  
+    res.render('compras',{title: "Compras"})
   });
-*/
+      
+  
+  
+
 module.exports = router;
 
 
